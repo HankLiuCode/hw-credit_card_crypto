1298f442a835b607e6810ba42bbb50bda2eeefb5
hank liu <hankliucode@gmail.com>
2024-03-11 00:36:20 +0800

    Shameless Green

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,11 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    check_digit = nums_a.pop
+    nums_sum = nums_a.reverse.each_with_index.map do |num, idx|
+      double_num = (num * 2) / 10 + (num * 2) % 10
+      idx.even? ? double_num : num
+    end.sum
+    (10 - nums_sum % 10) % 10 == check_digit
   end
 end
