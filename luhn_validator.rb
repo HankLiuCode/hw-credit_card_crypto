5a5ee17d8d079750eed3b12f6c3963983f230172
hank liu <hankliucode@gmail.com>
2024-03-14 22:00:08 +0800

    Fixed rubocop violations

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# LuhnValidator module
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -6,10 +9,16 @@ module LuhnValidator
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
     check_digit = nums_a.pop
-    nums_sum = nums_a.reverse.each_with_index.map do |num, idx|
-      double_num = (num * 2) / 10 + (num * 2) % 10
-      idx.even? ? double_num : num
+    checksum = generate_checksum(nums_a)
+    checksum == check_digit
+  end
+
+  def generate_checksum(nums)
+    nums_sum = nums.reverse.each_with_index.map do |num, idx|
+      double_num = num * 2
+      digit_sum = (double_num / 10 + double_num % 10)
+      idx.even? ? digit_sum : num
     end.sum
-    (10 - nums_sum % 10) % 10 == check_digit
+    (10 - nums_sum % 10) % 10
   end
 end
